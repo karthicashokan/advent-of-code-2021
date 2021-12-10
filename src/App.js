fba84cfb94f0524159a9a20ddc30e8ec6f75fecc
Karthic Ashokan <karthic@drivemotors.com>
2021-12-10 16:09:10 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
+const DAYS_COMPLETED_SO_FAR = 5;
+const DAYS = Array.from({length: DAYS_COMPLETED_SO_FAR}, (_, i) => i + 1);
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Advent of Code 2021</header>
+      <div className='App-container'>
+          <div className="day-selector">
+              {DAYS.map((day) => (
+                  <div className="button-big" key={day}>
+                      <div>{`Day ${day}`}</div>
+                      <div>
+                          <button onClick={() => { onClick(day, 1) }}>{`1`}</button>
+                          <button onClick={() => { onClick(day, 2) }}>{`2`}</button>
+                      </div>
+                  </div>
+              ))}
+          </div>
+          <div className="results">
+              <div className="column">1</div>
+              <div className="column">2</div>
+          </div>
+      </div>
     </div>
   );
 }
+
+ const onClick = (day, part) => {
+    console.log({ day, part });
+     const inputFilePath = `${window.location.origin}/inputs/day${day}.txt`;
+     const functionToRun = require(`./solutions/day${day}.js`)[`part${part}`];
+     fetch(inputFilePath)
+         .then(response => response.text())
+         .then(data => functionToRun(data));
+ }
 
 export default App;
---
+++ src/solutions/day1.js
@@ -0,0 +1,9 @@
+module.exports = {
+    part1: (input) => {
+        console.log('part 1', input);
+    },
+
+    part2: (input) => {
+        console.log('part 2', input);
+    },
+};
